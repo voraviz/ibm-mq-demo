6e9be6823f8a0922e3eca926cbba388cb653af4f
Voravit <voravitl@gmail.com>
2026-07-22 18:34:48 +0700

    build only linux/amd64

---
+++ all-in-one-app/build_jvm_container.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+CONTAINER_NAME=quay.io/voravitl/simple-mq-app
+PLATFORM=linux/amd64 #,linux/arm64
+TAG=build8 #otel,latest,hi
+DOCKERFILE=jvm #jvm,jvm-runtime,hummingbird
+IMAGE=$CONTAINER_NAME:$TAG
+CONTAINER_RUNTIME=podman
+podman --version 1>/dev/null 2>&1
+if [ $? -ne 0 ];
+then
+   CONTAINER_RUNTIME=docker 
+fi
+mvn clean package -DskipTests=true
+echo "Build with Dockerfile.$DOCKERFILE tag $TAG"
+$CONTAINER_RUNTIME build --platform $PLATFORM  \
+-f src/main/docker/Dockerfile.$DOCKERFILE -t $IMAGE .
+
