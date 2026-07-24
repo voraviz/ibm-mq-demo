fdcd06e88b764ecdc2925e9603efe7787a8ac08e
Voravit <voravitl@gmail.com>
2026-07-24 14:00:56 +0700

    Start Prometheus with config that scarpe from cluster configuration

--- obs-app/etc/start-prometheus.sh
+++ obs-app/etc/start-prometheus.sh
@@ -7,12 +7,12 @@ set -e
 
 echo "Starting Prometheus..."
 podman run --name prometheus \
-  -v "./prometheus.yaml:/etc/prometheus/prometheus.yaml:Z" \
+  -v "./prometheus.cluster.yaml:/etc/prometheus/prometheus.yaml:Z" \
   --network mq-ha-net \
   -p 9090:9090 \
   -e TZ=Asia/Bangkok \
   -d prom/prometheus:latest \
-  "--config.file=/etc/prometheus/prometheus.cluster.yaml"
+  "--config.file=/etc/prometheus/prometheus.yaml"
 
 echo ""
 echo "Prometheus started:"
